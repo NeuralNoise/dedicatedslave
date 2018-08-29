b7c93fda1c13a7cdc668d2a9a1973998e04a0f5b
Alexandre Ferreira <alex.jorge.m@gmail.com>
2018-08-29 21:09:28 +0100

    early env script to compile with dub

--- build/env.sh
+++ build/env.sh
@@ -1,74 +1,17 @@
 envvariable=".venv"
 
-function __cco() {
-	if [ -x "/bin/ldc" ] || [ -x "/usr/bin/ldc" ]; then
-		export DC=ldc;
-	elif [ -x "/bin/dmd" ] || [ -x "/usr/bin/dmd" ]; then
-		export DC=dmd;
-	fi;
-}
+#function i() { virtualenv --python=python3 "${envvariable}"; source ./${envvariable}/bin/activate; pip install -r requirements.txt; deactivate; }
+#function b() { source ./${envvariable}/bin/activate; meson; deactivate; }
+#function b() { dub build; dub build :gui; dub build :cli; }
+function b() { dub build; dub build :gui;}
+function bf() { dub build --force; dub build :gui --force;}
+function s() { src/gui/.out/bin/dedicatedslave-gui }
 
-function __pdep() {
-	dub build gtk-d:gtkd --compiler=$DC;
-}
+echo "Help:"
+#echo "‘i’ - Install"
+echo "‘b’ - Build"
+echo "‘bf’ - Build (Force)"
+echo "‘s’ - Start"
 
-echo "Development commands:"
-function __prepare() {
-	__cco; __pdep; meson $@ . .build
-}
-alias p="__prepare"
-echo "p\tPrepare"
-
-function __clear() {
-	if [[ $@ == "all" ]]; then
-		rm -rf .build .dub;
-	elif [[ $@ == "appcache" ]]; then
-		rm -rf .dedicatedslave ._dedicatedslave;
-	else
-		__build clean;
-	fi;
-}
-alias c="__clear"
-echo "c\tClear"
-
-function __build() {
-	if [[ ! -d ".build" ]]; then
-		(>&2 echo "error: no .build folder")
-	fi;
-	if [[ $@ == "force" ]]; then
-		pushd .build;
-			ninja clean;
-			ninja
-		popd;
-	else
-		pushd .build; ninja $@; popd;
-	fi;
-}
-alias b="__build"
-echo "b\tBuild"
-
-function __install() { __build install; }
-alias i="__install"
-echo "i\tInstall"
-
-function __start() { ./.build/src/gui/dedicatedslave-$@ }
-alias s="__start"
-echo "s\tStart"
-function __startdbg() { gdb ./.build/src/gui/dedicatedslave-$@ }
-alias sd="__startdbg"
-echo "sd\tStart Debug"
-
-echo "\nQuick commands:"
-alias breakfast="__prepare"
-echo "breakfast\tPrepare"
-function brunch() {
-	if [[ $@ == "clear" ]]; then
-		__clear all;
-	else
-		if [[ ! -d ".build" ]];then
-			__prepare;
-		fi;
-		__build $@;
-	fi;
-}
-echo "brunch\t\tEasy prepare & build"
+#dub
+#ldc
