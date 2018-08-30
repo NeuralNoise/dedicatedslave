3dfe881931195dd471d28c90ac28d139c4a03765
Luís Ferreira <contact@lsferreira.net>
2018-08-30 20:16:47 +0100

    fix access to steamcmd via relative path (now using runtime absolute path)
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- src/source/dedicatedslave/loader.d
+++ src/source/dedicatedslave/loader.d
@@ -6,25 +6,63 @@ import DedicatedSlave = dedicatedslave;
 
 class Loader {
 	this() {
-		if(!exists(DedicatedSlave.realPath))
+		{
+			version(Windows) {
+				import std.c.windows.windows;
+			} else version(OSX) {
+				extern(C) int _NSGetExecutablePath(char* buf, uint* bufsize);
+			} else version(linux) {
+				 import core.sys.posix.unistd : readlink;
+			} else {
+				static assert(0);
+			}
+			import std.conv;
+			import std.string;
+			import std.path;
+
+			static string cachedExecutablePath;
+			if (!cachedExecutablePath) {
+				char[4096] buf;
+				long filePathLength;
+		
+				version(Windows) {
+					filePathLength = GetModuleFileNameA(null, buf.ptr, buf.length - 1);
+					assert(filePathLength != 0);
+				}
+				else version(OSX) {
+					filePathLength = cast(long) (buf.length - 1);
+					int res = _NSGetExecutablePath(buf.ptr, &filePathLength);
+					assert(res == 0);
+				}
+				else version(linux) {
+					filePathLength = readlink(toStringz("/proc/self/exe"), buf.ptr, buf.length - 1);
+				}
+				else {
+					static assert(0);
+				}
+				cachedExecutablePath = to!string(buf[0 .. filePathLength]);
+			}
+			exe_path = cachedExecutablePath.dirName ~ "/";
+		}
+		if(!exists(exe_path~DedicatedSlave.realPath))
 			installEnvironment();
 
-		steamapi_instance = new SteamAPI(&_changeLogState);
+		steamapi_instance = new SteamAPI(this);
 	}
 
 	void installEnvironment()
 	{
 		_changeLogState("Installing the environment...");
 
-		if(exists(DedicatedSlave.tmpPath))
-			rmdirRecurse(DedicatedSlave.tmpPath);
-		mkdir(DedicatedSlave.tmpPath);
+		if(exists(exe_path~DedicatedSlave.tmpPath))
+			rmdirRecurse(exe_path~DedicatedSlave.tmpPath);
+		mkdir(exe_path~DedicatedSlave.tmpPath);
 
 		_changeLogState("Downloading " ~ DedicatedSlave.urlPlatform ~ "...");
 		import std.net.curl : download;
 		import std.path : extension;
 		immutable string steamcmd_extension = extension(DedicatedSlave.urlPlatform);
-		immutable string steamcmd_filename = DedicatedSlave.tmpPath~"steamcmd"~extension(DedicatedSlave.urlPlatform);
+		immutable string steamcmd_filename = exe_path~DedicatedSlave.tmpPath~"steamcmd"~extension(DedicatedSlave.urlPlatform);
 		download(DedicatedSlave.urlPlatform, steamcmd_filename);
 		
 		_changeLogState("Extracting " ~ steamcmd_filename ~ "...");
@@ -34,38 +72,39 @@ class Loader {
 			import std.stdio: writeln;
 
 			auto archive_file = new TarGzArchive(read(steamcmd_filename));
-			mkdir(DedicatedSlave.tmpPath~"steamcmd/");
+			mkdir(exe_path~DedicatedSlave.tmpPath~"steamcmd/");
 
 			foreach (memberFile; archive_file.directories)
 			{
 				_changeLogState("Create directory " ~ memberFile.path ~ "...");
-				mkdir(DedicatedSlave.tmpPath~"steamcmd/"~memberFile.path);
+				mkdir(exe_path~DedicatedSlave.tmpPath~"steamcmd/"~memberFile.path);
 				_changeLogState("Set attributes for " ~ memberFile.path ~ "...");
-				setAttributes(DedicatedSlave.tmpPath~"steamcmd/"~memberFile.path, memberFile.permissions);
+				setAttributes(exe_path~DedicatedSlave.tmpPath~"steamcmd/"~memberFile.path, memberFile.permissions);
 			}
 
 			foreach (memberFile; archive_file.files)
 			{
 				_changeLogState("Extracting " ~ memberFile.path ~ "...");
-				write(DedicatedSlave.tmpPath~"steamcmd/"~memberFile.path, memberFile.data);
+				write(exe_path~DedicatedSlave.tmpPath~"steamcmd/"~memberFile.path, memberFile.data);
 				_changeLogState("Set attributes for " ~ memberFile.path ~ "...");
-				setAttributes(DedicatedSlave.tmpPath~"steamcmd/"~memberFile.path, memberFile.permissions);
+				setAttributes(exe_path~DedicatedSlave.tmpPath~"steamcmd/"~memberFile.path, memberFile.permissions);
 			}
 		}
 		_changeLogState("Delete "~steamcmd_filename~"...");
 		remove(steamcmd_filename);
 
 		_changeLogState("Finishing setup...");
-		rename(DedicatedSlave.tmpPath, DedicatedSlave.realPath);
+		rename(exe_path~DedicatedSlave.tmpPath, exe_path~DedicatedSlave.realPath);
 	}
 
 	SteamAPI steamapi_instance;
+	string exe_path;
 
 protected:
 	void _internalLogger(immutable string msg) {}
 
 private:
-	void _changeLogState(immutable string msg)
+	package final void _changeLogState(immutable string msg)
 	{
 		import std.experimental.logger : info;
 		info(msg);
--- src/source/dedicatedslave/steamapi.d
+++ src/source/dedicatedslave/steamapi.d
@@ -1,14 +1,15 @@
 module dedicatedslave.steamapi;
 
 import DedicatedSlave = dedicatedslave;
+import dedicatedslave.loader;
 
 import core.thread;
 import std.process;
 
 class SteamAPI {
-	this(void delegate(immutable(string)) funcChangeLogState)
+	this(Loader loader)
 	{
-		_changeLogState = funcChangeLogState;
+		_loader = loader;
 		_thread_handler = new Thread(&createNewProcess).start();
 	}
 
@@ -18,14 +19,14 @@ private:
 
 	Thread _thread_handler;
 
-	void delegate(immutable(string)) _changeLogState;
+	Loader _loader;
 
 	void createNewProcess()
 	{
-		pipeproc_handler = pipeShell(DedicatedSlave.realPath~"steamcmd/"~DedicatedSlave.execPathPlatform~DedicatedSlave.execFilePlatform, Redirect.all, ["LD_LIBRARY_PATH":DedicatedSlave.realPath~"steamcmd/"~DedicatedSlave.execPathPlatform]);
+		pipeproc_handler = pipeShell(_loader.exe_path~DedicatedSlave.realPath~"steamcmd/"~DedicatedSlave.execPathPlatform~DedicatedSlave.execFilePlatform, Redirect.all, ["LD_LIBRARY_PATH":_loader.exe_path~DedicatedSlave.realPath~"steamcmd/"~DedicatedSlave.execPathPlatform]);
 		scope(exit) wait(pipeproc_handler.pid);
 
-		foreach (line; pipeproc_handler.stdout.byLine) _changeLogState(line.idup);
-		foreach (line; pipeproc_handler.stderr.byLine) _changeLogState(line.idup);
+		foreach (line; pipeproc_handler.stdout.byLine) _loader._changeLogState(line.idup);
+		foreach (line; pipeproc_handler.stderr.byLine) _loader._changeLogState(line.idup);
 	}
 }
