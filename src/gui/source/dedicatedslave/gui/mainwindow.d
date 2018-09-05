039161bf77ec5fe436ec6daf8f2b3ad00ca86d19
Luís Ferreira <contact@lsferreira.net>
2018-09-05 17:26:24 +0100

    gui: update the style of main window
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- src/gui/source/dedicatedslave/gui/mainwindow.d
+++ src/gui/source/dedicatedslave/gui/mainwindow.d
@@ -10,10 +10,110 @@ import gtk.MenuItem;
 import gtk.Menu;
 import gtk.Widget;
 import gtk.Button;
-
+import gtk.ListBox;
+import gtk.TextView;
+import gtk.Toolbar;
+import gtk.Statusbar;
+import gtk.Notebook;
 import gdk.Event;
-
 import dedicatedslave.gui.loader;
+
+class FileMenuItem : MenuItem
+{
+	this()
+	{
+		super("File");
+		_menu = new Menu();
+
+		_menuItem = new MenuItem("Exit");
+		_menuItem.addOnButtonRelease(&exit);
+		_menu.append(_menuItem);
+
+		setSubmenu(_menu);
+	}
+
+	bool exit(Event event, Widget widget)
+	{
+		Main.quit();
+		return true;
+	}
+
+private:
+	Menu _menu;
+	MenuItem _menuItem;
+}
+
+class EditMenuItem : MenuItem
+{
+	this()
+	{
+		super("Edit");
+		_menu = new Menu();
+
+		//_menuItem = new MenuItem("");
+		//_menuItem.addOnButtonRelease(&);
+		//_menu.append(_menuItem);
+
+		setSubmenu(_menu);
+	}
+private:
+	Menu _menu;
+	MenuItem _menuItem;
+}
+
+class ViewMenuItem : MenuItem
+{
+	this()
+	{
+		super("View");
+		_menu = new Menu();
+
+		//_menuItem = new MenuItem("");
+		//_menuItem.addOnButtonRelease(&);
+		//_menu.append(_menuItem);
+
+		setSubmenu(_menu);
+	}
+private:
+	Menu _menu;
+	MenuItem _menuItem;
+}
+
+class ToolsMenuItem : MenuItem
+{
+	this()
+	{
+		super("Tools");
+		_menu = new Menu();
+
+		//_menuItem = new MenuItem("");
+		//_menuItem.addOnButtonRelease(&);
+		//_menu.append(_menuItem);
+
+		setSubmenu(_menu);
+	}
+private:
+	Menu _menu;
+	MenuItem _menuItem;
+}
+
+class HelpMenuItem : MenuItem
+{
+	this()
+	{
+		super("Help");
+		_menu = new Menu();
+
+		_menuItem = new MenuItem("About");
+		//_menuItem.addOnButtonRelease(&);
+		_menu.append(_menuItem);
+
+		setSubmenu(_menu);
+	}
+private:
+	Menu _menu;
+	MenuItem _menuItem;
+}
 
 class MainWindow : ApplicationWindow
 {
@@ -21,43 +121,36 @@ class MainWindow : ApplicationWindow
 	{
 		super(application);
 		setTitle("DedicatedSlave");
-		// TODO: provavelmente isto esta a foder o aspecto do MenuBar, tenta meter a MenuBar na MainWindow
-		setBorderWidth(10);
 		setDefaultSize(800, 600);
 
 		MenuBar menuBar = new MenuBar();
-     	menuBar.append(new FileMenuItem());
+		menuBar.append(new FileMenuItem());
+		menuBar.append(new EditMenuItem());
+		menuBar.append(new ViewMenuItem());
+		menuBar.append(new ToolsMenuItem());
+		menuBar.append(new HelpMenuItem());
+
+		Toolbar toolbar = new Toolbar();
 		Box box = new Box(Orientation.VERTICAL, 10);
-    	box.packStart(menuBar, false, false, 0);
-		box.packStart(new Button("Hello World"), true, true, 0);
-		box.packStart(new Button("Click Me!"), true, false, 0);
-		box.packStart(new Button("Goodbye World"), false, false, 10);
+		Box hbox = new Box(Orientation.HORIZONTAL, 10);
+		Box left_vbox = new Box(Orientation.VERTICAL, 10);
+		Box right_vbox = new Box(Orientation.VERTICAL, 10);
+		box.packStart(menuBar, false, false, 0);
+		box.packStart(toolbar, false, false, 0);
+		hbox.setBorderWidth(8);
+		TextView terminalTextView = new TextView();
+		ListBox listBox = new ListBox();
+		left_vbox.packStart(terminalTextView, true, true, 0);
+		left_vbox.packStart(listBox, true, true, 0);
+		hbox.packStart(left_vbox, true, true, 0);
+		Notebook tabController = new Notebook();
+		right_vbox.packStart(tabController, true, true, 0);
+		hbox.packStart(right_vbox, true, true, 0);
+		box.packStart(hbox, true, true, 0);
+		Statusbar statusbar = new Statusbar();
+		box.packStart(statusbar, false, false, 0);
 		add(box);
 
 		showAll();
 	}
-}
-
-class FileMenuItem : MenuItem
-{
-    Menu fileMenu;
-    MenuItem exitMenuItem;
-   
-    this()
-    {
-        super("File");
-        fileMenu = new Menu();
-       
-        exitMenuItem = new MenuItem("Exit");
-        exitMenuItem.addOnButtonRelease(&exit);
-        fileMenu.append(exitMenuItem);
-       
-        setSubmenu(fileMenu);
-    }
-   
-    bool exit(Event event, Widget widget)
-    {
-        Main.quit();
-        return true;
-    }
 }
